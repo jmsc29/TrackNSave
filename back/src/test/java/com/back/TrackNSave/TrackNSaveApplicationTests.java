9720c515d3de23de641c69d615d3cf47b27cc99d
José María Sáez Castro <saezcastrojosemaria@gmail.com>
2024-08-15 14:47:54 +0200

    First commit; Created back and front folders

---
+++ back/src/test/java/com/back/TrackNSave/TrackNSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.TrackNSave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackNSaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
