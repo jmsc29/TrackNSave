bbd56a596be06310cf40acd3998c06e85bbeaa5b
José María Sáez Castro <saezcastrojosemaria@gmail.com>
2024-08-15 18:56:12 +0200

    volver a config por defecto

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build', // Cambia 'build' a la carpeta que desees
-  },
   plugins: [react()],
 })
